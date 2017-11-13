27097d3adcb4aed806efe456567a7643da756917
Tyler Spadgenske <tyspadgens@gmail.com>
2017-11-13 16:05:44 -0600

    updates

--- firmware/TyroBot/examples/original-firmware/original-firmware.ino
+++ firmware/TyroBot/examples/original-firmware/original-firmware.ino
@@ -1,4 +1,4 @@
-/* TyroBot Demo Code
+/* TyroBot Demo Code v1.0.0
  *  copyright (c) 2017 Tyro Electronics
  *  written by Tyler Spadgenske
  *  GNU General Public License 3.0
@@ -6,8 +6,8 @@
 
 #include <TyroBot.h> //include this library whenever using TyroBot
 
-char ssid[] = "spadgenske";
-char pass[] = "68mustang";
+char ssid[] = "spadgenske"; //Wifi SSID 
+char pass[] = "68mustang"; //Wifi password
 
 Adafruit_LIS3DH lis = Adafruit_LIS3DH(); //Setup the Accelerometer
 Adafruit_ILI9341 tft = Adafruit_ILI9341(4, 9, 23, 24, -1, 22); //CS, DC, MOSI, CLK, RST, MISO (Setup the display)
@@ -26,9 +26,13 @@ void loop()
   bot.menu(); //display the default splash screen on startup
   
   //Any code below runs when "run program" is tapped in TyroBot's menu
+  bot.token = "b8354jvkbG7ZSitd6ygT4z"; //IFTT Key (you will need to use your own)
+  bot.IFTT("test");//event trigger
+  bot.shakeHead();
+  bot.waveRightArm(1);
+  bot.waveLeftArm(1);
   bot.forward(4);
   bot.turnRight(2);
   bot.shakeHead();
   bot.liftRightFoot();
 }
-
